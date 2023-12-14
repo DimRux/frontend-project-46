ebf6179b9944f5c6c5dbfe15ded8e9d9b31406e8
dimrux <diman777.10@mail.ru>
2023-12-14 21:41:17 +0300

    fixed formatters.index.js

--- formatters/formatters.index.js
+++ formatters/formatters.index.js
@@ -3,11 +3,11 @@ import stylish from './stylish.js';
 import json from './json.js';
 
 export default (formatName) => {
-  if (formatName === 'stylish') {
-    return stylish;
+  if (formatName === 'plain') {
+    return plain;
   }
   if (formatName === 'json') {
     return json;
   }
-  return plain;
+  return stylish;
 };
