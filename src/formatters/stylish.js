76bbb1f0b8b6efb9da37c45105a402a1eaf0115a
dimrux <diman777.10@mail.ru>
2023-12-27 22:17:53 +0300

    refactoring stylish

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,35 +1,11 @@
 import _ from 'lodash';
 
-const convertArrInStr = (value) => {
-  if (_.isObject(value) && !Array.isArray(value)) {
-    const lines = Object
-      .entries(value)
-      .map(([key, val]) => {
-        if (Array.isArray(val)) {
-          return `${key}: [${convertArrInStr(val)}]`;
-        }
-        return `${key}: {${convertArrInStr(val)}}`;
-      });
-    return lines.join('');
-  }
-  const result = value.map((el) => {
-    if (Array.isArray(el)) {
-      return `[${convertArrInStr(el)}]`;
-    }
-    if (_.isObject(el)) {
-      return `{${convertArrInStr(el)}}`;
-    }
-    return `${el}`;
-  });
-  return result.join(', ');
-};
-
 const stringify = (value, depth = 1) => {
   if (!_.isObject(value)) {
     return `${value}`;
   }
   if (Array.isArray(value)) {
-    return `[${convertArrInStr(value)}]`;
+    return `[${value.toString()}]`;
   }
   const indentSize = depth + 1;
   const currentIndent = '  '.repeat(indentSize + 1);
@@ -52,7 +28,7 @@ const getIndentation = (depth, sign) => {
 
 const mapping = {
   changed: (el, depth) => `${getIndentation(depth, '-')} ${el.keyName}: ${stringify(el.value1, depth + 1)}\n${getIndentation(depth, '+')} ${el.keyName}: ${stringify(el.value2, depth + 1)}`,
-  nested: (el, depth) => `${getIndentation(depth, ' ')} ${el.keyName}: ${el.children.map((node) => `\n${mapping[node.status](node, depth + 2)}`)}`,
+  nested: (el, depth) => `${getIndentation(depth, ' ')} ${el.keyName}: {\n${el.children.map((node) => `${mapping[node.status](node, depth + 2)}`).join('\n')}\n${'  '.repeat(depth + 1)}}`,
   equals: (el, depth) => `${getIndentation(depth, ' ')} ${el.keyName}: ${stringify(el.value, depth + 1)}`,
   minus: (el, depth) => `${getIndentation(depth, '-')} ${el.keyName}: ${stringify(el.value, depth + 1)}`,
   plus: (el, depth) => `${getIndentation(depth, '+')} ${el.keyName}: ${stringify(el.value, depth + 1)}`,
